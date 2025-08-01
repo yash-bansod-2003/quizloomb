345ef767307b58a332e545a70ab2ec40321b4f9f
YashB <yashbansodgit@gmail.com>
2025-08-01 23:27:37 +0530

    refactor: improve code formatting and consistency in QuizSession and quizSessions.service

--- src/entities/QuizSession.ts
+++ src/entities/QuizSession.ts
@@ -1,12 +1,12 @@
 import {
-      Column,
-      Entity,
-      ManyToOne,
-      CreateDateColumn,
-      UpdateDateColumn,
-      PrimaryColumn,
-      BeforeInsert,
-      OneToOne,
+  Column,
+  Entity,
+  ManyToOne,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryColumn,
+  BeforeInsert,
+  OneToOne,
 } from "typeorm";
 import { generateId } from "better-auth";
 import { Result } from "@/entities/Result.js";
@@ -14,35 +14,35 @@ import { Quiz } from "./Quiz.js";
 
 @Entity("quiz_sessions")
 export class QuizSession {
-      @PrimaryColumn({ type: "text" })
-      id: string;
+  @PrimaryColumn({ type: "text" })
+  id: string;
 
-      @BeforeInsert()
-      beforeInsert() {
-            const id = generateId();
-            this.id = id;
-      }
+  @BeforeInsert()
+  beforeInsert() {
+    const id = generateId();
+    this.id = id;
+  }
 
-      @Column({ type: "timestamp" })
-      expiry: Date;
+  @Column({ type: "timestamp" })
+  expiry: Date;
 
-      @Column({ type: "jsonb" })
-      fields: string;
+  @Column({ type: "jsonb" })
+  fields: string;
 
-      @Column("text", { array: true })
-      questions: string[];
+  @Column("text", { array: true })
+  questions: string[];
 
-      @ManyToOne(() => Quiz, (quiz) => quiz.quizSessions, {
-            onDelete: "CASCADE",
-      })
-      quiz: Quiz;
+  @ManyToOne(() => Quiz, (quiz) => quiz.quizSessions, {
+    onDelete: "CASCADE",
+  })
+  quiz: Quiz;
 
-      @OneToOne(() => Result, (result) => result.quizSession)
-      result: Result;
+  @OneToOne(() => Result, (result) => result.quizSession)
+  result: Result;
 
-      @CreateDateColumn({ type: "timestamp" })
-      createdAt: Date;
+  @CreateDateColumn({ type: "timestamp" })
+  createdAt: Date;
 
-      @UpdateDateColumn({ type: "timestamp" })
-      updatedAt: Date;
+  @UpdateDateColumn({ type: "timestamp" })
+  updatedAt: Date;
 }
